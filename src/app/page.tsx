c7df2824e4d1bb15227d1c11966cc6c2737a4539
nandani <reach.nandanipaliwal@gmail.com>
2024-07-18 10:21:48 +0530

    feat: usp section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Portfolio from "~/components/portfolio-section";
-
+import Portfolio from '~/components/portfolio-section'
+import UspSection from '~/components/usp-section'
 
 export default function Homr(): JSX.Element {
-  return (
-    <main className=" dark:bg-darkGrey bg-white">
-<Portfolio />
-    </main>
-  );
+	return (
+		<main className=" bg-white dark:bg-darkGrey">
+			<Portfolio />
+			<UspSection />
+		</main>
+	)
 }
--- src/components/design-system/heading.tsx
+++ src/components/design-system/heading.tsx
@@ -1,31 +1,27 @@
-import React from "react";
-import { clsx } from "clsx";
+import React from 'react'
+import { clsx } from 'clsx'
 
 export interface HeadingProps {
-  as?: keyof JSX.IntrinsicElements;
-  type?: "h1" | "h2" | "h3" | "h4";
-  className?: string;
-  children: React.ReactNode;
+	as?: keyof JSX.IntrinsicElements
+	type?: 'h1' | 'h2' | 'h3' | 'h4'
+	className?: string
+	children: React.ReactNode
 }
 
 const Heading: React.FC<HeadingProps> = ({
-  as: Heading = "h2",
-  type = "",
-  className = "",
-  children,
+	as: Heading = 'h2',
+	type = '',
+	className = '',
+	children
 }) => {
-  const headingStyles: { [key: string]: string } = {
-	h1: 'text-[64px] leading-[76px] font-semibold',
-    h2: "text-4xl leading-[44px] font-semibold",
-    h3: "text-[28px]  leading-9 font-semibold",
-    h4: "text-xl leading-[28px] font-semibold",
-  };
+	const headingStyles: { [key: string]: string } = {
+		h1: 'text-h1 font-semibold',
+		h2: 'text-h2 font-semibold',
+		h3: 'text-h3 font-semibold',
+		h4: 'text-h4 font-semibold'
+	}
 
-  return (
-    <Heading className={clsx(headingStyles[type], className)}>
-      {children}
-    </Heading>
-  );
-};
+	return <Heading className={clsx(headingStyles[type], className)}>{children}</Heading>
+}
 
-export { Heading };
+export { Heading }
